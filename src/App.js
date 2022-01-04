7bc5e08e4197eb609bac1d2987dbbe707b4b4e7f
ramon oliveira <ramon30012000@gmail.com>
2022-01-04 19:47:31 -0300

    routes created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 function App () {
     return (
-        <h1>sae</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<h1>boa</h1>}/>
+                {/* <Route path="/cadastro" element={}/>
+                <Route path="/habitos" element={}/>
+                <Route path="/hoje" element={}/>
+                <Route path="/historico" element={}/> */}
+            </Routes>
+        </BrowserRouter>
     )
 }
 
