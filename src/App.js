0acc638db69bd57d8a955a92ae9c345d8c943f46
ramon oliveira <ramon30012000@gmail.com>
2022-01-04 19:29:18 -0300

    project initiated

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App () {
+    return (
+        <h1>sae</h1>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+import "./css/reset.css";
+import "./css/styles.css";
+
+ReactDOM.render(<App/>, document.querySelector(".root"));
